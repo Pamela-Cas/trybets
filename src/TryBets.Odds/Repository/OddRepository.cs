971a0fc491f5d3d26eec01897b37b0c16e2407a0
Pamela-Cas <pxcx611@gmail.com>
2024-03-22 15:45:40 -0300

    subindo projeto

--- src/TryBets.Odds/Repository/OddRepository.cs
+++ src/TryBets.Odds/Repository/OddRepository.cs
@@ -15,34 +15,6 @@ public class OddRepository : IOddRepository
 
     public Match Patch(int MatchId, int TeamId, string BetValue)
     {
-       string BetValueConverted = BetValue.Replace(',', '.');
-       decimal BetValueDecimal = Decimal.Parse(BetValueConverted, CultureInfo.InvariantCulture);
-       
-       var match = _context.Matches.FirstOrDefault(m => m.MatchId == MatchId);
-       var team = _context.Teams.FirstOrDefault(t => t.TeamId == TeamId);
-       
-        if (match == null || team == null)
-        {
-        throw new Exception("Error");
-        }
-        
-        if (match.MatchTeamAId != TeamId && match.MatchTeamBId != TeamId)
-        {
-            throw new Exception("Team is not in this match");
-        }
-
-        if (match.MatchTeamAId == TeamId)
-        {
-            match.MatchTeamAValue += BetValueDecimal;
-        }
-        else if (match.MatchTeamBId == TeamId)
-        {
-            match.MatchTeamBValue += BetValueDecimal;
-        }
-
-        _context.Matches.Update(match);
-        _context.SaveChanges();
-
-        return match;
+        throw new NotImplementedException();
     }
 }
