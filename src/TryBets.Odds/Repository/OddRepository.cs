7032664a11f735e41d9e043c50e83e4ef70aa59a
Pamela-Cas <pxcx611@gmail.com>
2024-03-22 15:42:19 -0300

    req4

--- src/TryBets.Odds/Repository/OddRepository.cs
+++ src/TryBets.Odds/Repository/OddRepository.cs
@@ -15,6 +15,34 @@ public class OddRepository : IOddRepository
 
     public Match Patch(int MatchId, int TeamId, string BetValue)
     {
-        throw new NotImplementedException();
+       string BetValueConverted = BetValue.Replace(',', '.');
+       decimal BetValueDecimal = Decimal.Parse(BetValueConverted, CultureInfo.InvariantCulture);
+       
+       var match = _context.Matches.FirstOrDefault(m => m.MatchId == MatchId);
+       var team = _context.Teams.FirstOrDefault(t => t.TeamId == TeamId);
+       
+        if (match == null || team == null)
+        {
+        throw new Exception("Error");
+        }
+        
+        if (match.MatchTeamAId != TeamId && match.MatchTeamBId != TeamId)
+        {
+            throw new Exception("Team is not in this match");
+        }
+
+        if (match.MatchTeamAId == TeamId)
+        {
+            match.MatchTeamAValue += BetValueDecimal;
+        }
+        else if (match.MatchTeamBId == TeamId)
+        {
+            match.MatchTeamBValue += BetValueDecimal;
+        }
+
+        _context.Matches.Update(match);
+        _context.SaveChanges();
+
+        return match;
     }
 }
